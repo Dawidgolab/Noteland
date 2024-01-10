46fb5e4f340c82ddd3a041d44c7eca761843d76c
Dawidgolab <dawidg122@interia.pl>
2024-01-10 15:23:13 +0100

    Updated: -> home page from /home to / -> create new feature : A back button(html, css , javascript) -> improve the width of the text area

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -4,26 +4,48 @@ document.addEventListener('DOMContentLoaded', function() {
   var circle3 = document.querySelector('.circle3');
   var notesContainer = document.querySelector('.notes-container');
   var circleSave = document.querySelector('.circle-save');
+  var backButton = document.querySelector('.fas.fa-chevron-left');
 
   circle.addEventListener('click', function() {
-    circle.style.display = 'none'; // Hide circle1
-    circle2.style.display = 'block'; // Make circle2 visible
-    circle3.style.display = 'block'; // Make circle3 visible
-
-    circle2.style.left = '30%'; // Adjust the left position for circle2
-    circle3.style.left = '70%'; // Adjust the left position for circle3
+    circle.style.display = 'none';
+    circle2.style.display = 'block';
+    circle3.style.display = 'block';
+    circle2.style.left = '30%';
+    circle3.style.left = '70%';
+    localStorage.setItem('currentPage', 'circle2');
   });
 
   circle3.addEventListener('click', function() {
-    circle.style.display = 'none'; // Hide circle1
-    circle2.style.display = 'none'; // Hide circle2
-    circle3.style.display = 'none'; // Hide circle3
-    notesContainer.style.display = 'block'; // Show notes form
+    circle.style.display = 'none';
+    circle2.style.display = 'none';
+    circle3.style.display = 'none';
+    notesContainer.style.display = 'block';
     circleSave.style.display = 'block';
+    localStorage.setItem('currentPage', 'notesContainer');
   });
 
-  // Initially hide circle2, circle3, and notes container
-  if (circle2 && circle3 && notesContainer && circleSave) {
+  backButton.addEventListener('click', function() {
+    var currentPage = localStorage.getItem('currentPage');
+    if (currentPage === 'circle2' || currentPage === 'circle3') {
+      circle.style.display = 'flex';
+      circle2.style.display = 'none';
+      circle3.style.display = 'none';
+      notesContainer.style.display = 'none';
+      circleSave.style.display = 'none';
+      localStorage.setItem('currentPage', 'circle');
+    } else if (currentPage === 'notesContainer') {
+      circle.style.display = 'none';
+      circle2.style.display = 'block';
+      circle3.style.display = 'block';
+      notesContainer.style.display = 'none';
+      circleSave.style.display = 'none';
+      localStorage.setItem('currentPage', 'circle2');
+    }
+  });
+
+  hideElements();
+
+  function hideElements() {
     circle2.style.display = 'none';
     circle3.style.display = 'none';
     notesContainer.style.display = 'none';
